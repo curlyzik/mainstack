81df73484c649b281bdcdb1fea2ab5a0337c156e
curlyzik <curlyzik@gmail.com>
2023-04-24 16:25:07 +0100

    Updated _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { LayoutProps } from "@/typings";
+import type { AppProps } from "next/app";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: LayoutProps }) {
+  const getLayout = Component.getLayout ?? ((page: React.ReactElement) => page);
+
+  return (
+    <React.Fragment>{getLayout(<Component {...pageProps} />)}</React.Fragment>
+  );
 }
